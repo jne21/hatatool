f67b51021dfeda4033183127b6b724c4ac95aa6b
jne21 <github@jne21.com>
2016-01-10 15:00:08 +0200

    cms index page content support

--- cms/index.php
+++ cms/index.php
@@ -4,11 +4,16 @@ require('inc/authent.php');
 
 use common\Page;
 use CMS\RendererCMS as Renderer;
+use CMS\I18n;
 
-$pTitle = "Керуючий інтерфейс";
-
+$i18n = new I18n($registry->get('cms_i18n_path') . 'index.xml');
 $renderer = new Renderer(Page::MODE_NORMAL);
-$renderer->page->title = $pTitle;
 
+$pTitle = $i18n->getText('title');
+$renderer->page->set('title', $pTitle)
+    ->set('h1', $pTitle)
+    ->set('content', '');
+
+$renderer->loadPage();
 $renderer->output();
 
