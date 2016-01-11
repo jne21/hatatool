538cb955be5e90cf568a94802d1cd088524eace8
jne21 <github@jne21.com>
2016-01-11 13:53:48 +0200

    language support

--- classes/CMS/I18n.php
+++ classes/CMS/I18n.php
@@ -5,6 +5,6 @@ use \common\Registry;
 class I18n extends \common\I18n {
     function __construct($xml) {
         parent::__construct($xml);
-        $this->language = Registry::getInstance()->get('setup')->get('cms_locale'); 
+        $this->language = Registry::getInstance()->get('cms_locale'); 
     }
 }
--- cms/inc/variables.php
+++ cms/inc/variables.php
@@ -1,11 +1,18 @@
 <?php
-	$registry->set('cms_i18n_path', $site_root_absolute.'cms/i18n/');
-    if (isset($_SESSION['admin']['locale'])) {
-        $registry->set('i18n_language', $_SESSION['admin']['locale']);
-    }
+$registry->set('cms_i18n_path', $site_root_absolute.'cms/i18n/');
+
+if (isset($_SESSION['admin']['locale'])) {
+    $language = $_SESSION['admin']['locale'];
+}
+else {
+    $language = Registry::getInstance()->get('setup')->get('cms_locale');
+}
+
+$registry->set('i18n_language', $language);
+$registry->set('cms_locale', $language);
+
 //  $registry->set('cms_template_path', $site_root_absolute.'cms/tpl/');
-    $registry->set(
-            'cms_template_path',
-            $registry->get('cms_i18n_path').'template/'.$registry->get('i18n_language').'/'
-    );
-    
+$registry->set(
+        'cms_template_path',
+        $registry->get('cms_i18n_path').'template/'.$registry->get('i18n_language').'/'
+);
