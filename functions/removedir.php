7094a5a50b04ebd73fc9a0d2d554aac58d9ee422
kutuzove-hyuna <kutuzov.evgeniy@hyuna.bb>
2016-01-25 16:38:35 +0200

    HataTool Engine base functions

---
+++ functions/removedir.php
@@ -0,0 +1,16 @@
+<?php
+function removedir($name) {
+	if (is_dir($name)) {
+		if ($dir = opendir($name)) {
+			while (($file = readdir($dir)) !== false) {
+			  $newpath = "$name/$file";
+				if (is_dir($newpath)) {
+					if (substr($file,-1,1)!=".") removedir($newpath);
+				}
+				else unlink($newpath);
+			}
+			closedir($dir);
+		}
+		if (substr($file,-1,1)!=".") rmdir($name);
+	}
+}
