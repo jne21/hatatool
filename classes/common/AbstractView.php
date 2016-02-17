e2f1969fc050ef56b2a0164bc182c11453f65647
kutuzove-hyuna <kutuzov.evgeniy@hyuna.bb>
2016-02-18 00:29:41 +0200

    2016-02-18-0029

--- classes/common/AbstractView.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace common;
-
-abstract class AbstractView {
-
-    abstract static function get($data);
-
-}
--- classes/common/Renderer.php
+++ classes/common/Renderer.php
@@ -7,45 +7,42 @@ use \common\Template;
 
 class Renderer {
 
-	public
-		$page,
-		$content,
-		$template;
+    public
+        $page,
+        $content,
+        $template;
 
-	function __construct($content, $page = null) {
-	    $this->content = $content;
-	    if (isset($page)) {
-		    $this->page = $page;
+    function __construct($content, $page = null) {
+        $this->content = $content;
+        if (isset($page)) {
+            $this->page = $page;
             $this->loadPage();
-	    }
-	    else {
-	        $this->page = new Page();
-	    }
-	}
+        }
+        else {
+            $this->page = new Page();
+        }
+    }
 
-	function loadPage() {
-        $registry = Registry::getInstance();
-	    $this->updateContent([
-	            'content'  => $this->page->get('content'),
-	            'metaTags' => $this->page->get('metaTags'),
-	            'title'    => $this->page->get('metaTitle'),
-	            // render globals
-	            'site_root' => $registry->get('site_root')
-	    ]);
-	}
-	
-	function output() {
-		foreach ($this->page->get('headers') as $header => $forced) {
-	        header($header, $forced);
-	    }
-	    header('Content-Length: '.strlen($this->content));
-		echo $this->content;
-	}
+    function loadPage() {
+        $this->updateContent([
+            'content'  => $this->page->get('content'),
+            'metaTags' => $this->page->get('metaTags'),
+            'title'    => $this->page->get('metaTitle')
+        ]);
+    }
 
-	function updateContent($data) {
-		$tpl = new Template($this->content);
-		$this->content = $tpl->apply($data);
-		return $this;
-	}
+    function output() {
+        foreach ($this->page->get('headers') as $header => $forced) {
+            header($header, $forced);
+        }
+        header('Content-Length: '.strlen($this->content));
+        echo $this->content;
+    }
+
+    function updateContent($data) {
+        $tpl = new Template($this->content);
+        $this->content = $tpl->apply($data);
+        return $this;
+    }
 
 }
